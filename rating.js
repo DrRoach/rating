c204b13fd4ef683b6f1d6d4e5042a84857f63b38
DrRoach <ryandeas1@gmail.com>
2015-01-08 23:14:55 +0000

    Added classes and added mouseout feature

--- rating.js
+++ rating.js
@@ -19,10 +19,26 @@ rating.create = function(settings){
     //Set the default name
     this.name = settings.name || rating.name;
 
+    //Set the classes
+    var ratingClass = settings.ratingClass || {};
+    console.log(ratingClass);
+
     var html = '<input type="hidden" name="'+this.name+'" value="'+this.defaultRating+'">';
     //Create the ratings HTML
     for(var i = 0; i < this.defaultRating; i++) {
-        html = html + '<img src="'+this.selectedIcon+'" data-position="'+(i+1)+'">';
+        html = html + '<img src="'+this.selectedIcon+'" data-position="'+(i+1)+'"';
+        for(var x = 0; x < ratingClass.length; x++) {
+            if(x === 0) {
+                html = html + ' class="';
+            }
+            html = html + ratingClass[x];
+            if(x+1 == ratingClass.length) {
+                html = html + '"';
+            } else {
+                html = html + ' ';
+            }
+        }
+        html = html + '>';
     }
     for(var i = 0; i < this.outOf - this.defaultRating; i++) {
         html = html + '<img src="'+this.unselectedIcon+'" data-position="'+(this.defaultRating+i+1)+'">';
@@ -32,6 +48,17 @@ rating.create = function(settings){
         var position = $(this).data('position');
         $(settings.selector + ' img').each(function(i, e) {
             if(i < position) {
+                $(e).attr('src', rating.selectedIcon);
+            } else {
+                $(e).attr('src', rating.unselectedIcon);
+            }
+        });
+    });
+
+    $(settings.selector + ' img').on('mouseout', function() {
+        var selected = $(this).siblings('input[name='+rating.name+']').val();
+        $(settings.selector + ' img').each(function(i, e) {
+            if(i < selected) {
                 $(e).attr('src', rating.selectedIcon);
             } else {
                 $(e).attr('src', rating.unselectedIcon);
