21a78eb1621ae071dda972667eb83310a4c7c5ad
Ryan Deas <ryan.deas@tecmark.co.uk>
2015-03-04 13:00:59 +0000

    Added support for font awesome

--- rating.js
+++ rating.js
@@ -22,10 +22,21 @@ rating.create = function(settings){
     //Set the classes
     var ratingClass = settings.ratingClass || {};
 
+    //Check whether the user is using font awesome
+    var usingFa = false;
+    var startingHtml = '<img src="';
+    var subSelector = 'img';
+    if(this.selectedIcon.substring(0, 3) == 'fa ' || this.unselectedIcon.substring(0, 3) == 'fa ') {
+        usingFa = true;
+        subSelector = 'i';
+        startingHtml = '<i class="';
+    }
+
+
     var html = '<input type="hidden" name="'+this.name+'" value="'+this.defaultRating+'">';
     //Create the ratings HTML
     for(var i = 0; i < this.defaultRating; i++) {
-        html = html + '<img src="'+this.selectedIcon+'" data-position="'+(i+1)+'"';
+        html = html + startingHtml+this.selectedIcon+'" data-position="'+(i+1)+'"';
         for(var x = 0; x < ratingClass.length; x++) {
             if(x === 0) {
                 html = html + ' class="';
@@ -38,14 +49,20 @@ rating.create = function(settings){
             }
         }
         html = html + '>';
+        if(usingFa) {
+            html = html + '</i>';
+        }
     }
     for(var i = 0; i < this.outOf - this.defaultRating; i++) {
-        html = html + '<img src="'+this.unselectedIcon+'" data-position="'+(this.defaultRating+i+1)+'">';
+        html = html + startingHtml+this.unselectedIcon+'" data-position="'+(this.defaultRating+i+1)+'">';
+        if(usingFa) {
+            html = html + '</i>';
+        }
     }
     $(settings.selector).html(html);
-    $(settings.selector + ' img').on('mouseover', function() {
+    $(settings.selector + ' ' + subSelector).on('mouseover', function() {
         var position = $(this).data('position');
-        $(settings.selector + ' img').each(function(i, e) {
+        $(settings.selector + ' ' + subSelector).each(function(i, e) {
             if(i < position) {
                 $(e).attr('src', rating.selectedIcon);
             } else {
@@ -54,9 +71,9 @@ rating.create = function(settings){
         });
     });
 
-    $(settings.selector + ' img').on('mouseout', function() {
+    $(settings.selector + ' ' + subSelector).on('mouseout', function() {
         var selected = $(this).siblings('input[name='+rating.name+']').val();
-        $(settings.selector + ' img').each(function(i, e) {
+        $(settings.selector + ' ' + subSelector).each(function(i, e) {
             if(i < selected) {
                 $(e).attr('src', rating.selectedIcon);
             } else {
@@ -65,7 +82,7 @@ rating.create = function(settings){
         });
     });
 
-    $(settings.selector + ' img').on('click', function() {
+    $(settings.selector + ' ' + subSelector).on('click', function() {
         $(this).siblings('input[name='+rating.name+']').val($(this).data('position'));
     });
 }
